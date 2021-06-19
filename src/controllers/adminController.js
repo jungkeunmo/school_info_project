b600ef1878b380f36b01833b596685990b0fb9cb
“jungkeunmo“ <abc59684495@gmail.com>
2021-06-19 17:10:05 +0900

    crud

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,7 +1,36 @@
+import Student from "../models/Student";
+
 export const classController = (req, res) => {
-    res.reder("admin/class");
+    res.render("admin/class");
 };
 
 export const studentController = (req, res) => {
-    res.reder("admin/student");
+    res.render("admin/student");
+};
+
+export const createStudentController = async (req, res) => {
+    const { body: {
+        stu_name,
+        stu_gender,
+        stu_school,
+        stu_grade,
+        stu_class,
+        stu_hobby,
+    }, } = req;
+    try {
+        const result = await Student.create({
+            name: stu_name,
+            gender: stu_gender,
+            school: stu_school,
+            grade: parseInt(stu_grade),
+            ban: parseInt(stu_class),
+            birth: "2003/20/20",
+            hobby: stu_hobby,
+        })
+
+        res.render("admin/student", { msg: "데이터가 추가되었습니다." });
+    } catch (e) {
+        console.log(e)
+        studentController(req, res);
+    }
 };
--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
     classController,
     studentController,
+    createStudentController,
 } from "../controllers/adminController";
 
 const adminRouter = express.Router();
 
 adminRouter.get("/class", classController);
 adminRouter.get("/student", studentController);
+adminRouter.post("/creatStudent", createStudentController)
 
 export default adminRouter;
